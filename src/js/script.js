95918f3a1d3956629ddcea4caf6cb19bb3f9b442
marcoss741-0 <marcossn741@icloud.com>
2025-02-10 13:06:59 -0300

    Implementei funcionalidade de adicionar card na coluna

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,74 @@
 const cards = document.querySelectorAll(".card");
 const columns = document.querySelectorAll(".cards");
+const addButton = document.querySelectorAll(".add-button");
+const modal = document.querySelector(".modal");
+
+let tkn = "teste";
+let desc = "teste";
+let tgn = ["teste", "teste2"];
+
+function setCard(e) {
+  modal.style.display = "flex";
+  const form = document.querySelector("#cardForm");
+  const cancelButton = document.querySelector(".cancel");
+
+  const closeModal = (e) => {
+    modal.style.display = "none";
+    form.reset();
+  };
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    // Pega os valores do form
+    const dados = new FormData(form);
+
+    tkn = dados.get("titulo") || "";
+    desc = dados.get("descricao") || "";
+
+    const tagsValue = dados.get("tags");
+    tgn = tagsValue
+      ? tagsValue
+          .split(",")
+          .map((tag) => tag.trim())
+          .filter((tag) => tag !== "")
+      : [];
+
+    //cria o card e adiciona na coluna recebendo os valores do form
+    const column =
+      e.target.parentElement.parentElement.parentElement.querySelector(
+        ".cards"
+      );
+
+    const card = document.createElement("div");
+    card.classList.add("card");
+    card.draggable = true;
+    card.innerHTML = `
+
+
+    <h3>${tkn}</h3>
+    <p>${desc}</p>
+    <div class="tags">
+      ${tgn.map((tgn) => `<span>${tgn}</span>`).join("")}
+    </div>
+  `;
+
+    column.appendChild(card);
+
+    // Adiciona os eventos de drag and drop no novo card
+    card.addEventListener("dragstart", dragStart);
+    card.addEventListener("dragend", dragEnd);
+
+    closeModal();
+  });
+
+  cancelButton.addEventListener("click", closeModal);
+}
+
+addButton.forEach((button) => {
+  button.addEventListener("click", setCard);
+});
+
+//funções de drag and drop
 
 function dragStart(event) {
   event.currentTarget.classList.add("is-dragging");
